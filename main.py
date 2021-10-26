76f3236b27e4e6a78d850c423381719eef3669b2
Christopher Truluck <ctruluck12107@gmail.com>
2021-10-26 15:00:42 +0000

    Fix title screen.

--- main.py
+++ main.py
@@ -14,23 +14,25 @@ colors = {
     "purple":35
 }
 
+print(colors['red'])
+
 def title_screen():
     global e
     global colors
     title = "Welcome to"
 
     world.clear()
-    print(f"{title} .", flush = True)
+    print(f"{title} .")
     sleep(1)
     world.clear()
-    print(f"{title} . .", flush = True)
+    print(f"{title} . .")
     sleep(1)
     world.clear()
-    print(f"{title} . . .", flush = True)
+    print(f"{title} . . .")
     sleep(1)
     # UwU Knot me daddy ~
-    
-    for i in range(9):
+
+    for i in range(9):  #  Bad colored text code. Very bad, extremely slow. 
         world.clear()
         print(f"\033[1;{i + 30};40mB", end="")
         print(f"\033[1;{i + 29};40mL", end="")
@@ -43,25 +45,27 @@ def title_screen():
         print(f"\033[1;{i + 23};40mS", end="")
         print(f"\033[1;{i + 22};40mH\n", end="")
         sleep(0.2)
+        print("\033[0m",end="")
 
     play = ""
 
-    print(f"\033[1;{colors.get(random.choice(e))}40mS",end="")
-    print(f"\033[1;{colors.get(random.choice(e))}40mt",end="")
-    print(f"\033[1;{colors.get(random.choice(e))}40ma",end="")
-    print(f"\033[1;{colors.get(random.choice(e))}40mr",end="")
-    print(f"\033[1;{colors.get(random.choice(e))}40mt",end="")
+    print(f"\033[1;{random.choice(list(colors.values()))}40mS",end="")
+    print(f"\033[1;{random.choice(list(colors.values()))}40mt",end="")
+    print(f"\033[1;{random.choice(list(colors.values()))}40ma",end="")
+    print(f"\033[1;{random.choice(list(colors.values()))}40mr",end="")
+    print(f"\033[1;{random.choice(list(colors.values()))}40mt",end="")
     print(" ",end="")
-    print(f"\033[1;{colors.get(random.choice(e))}40mG",end="")
-    print(f"\033[1;{colors.get(random.choice(e))}40ma",end="")
-    print(f"\033[1;{colors.get(random.choice(e))}40mm",end="")
-    print(f"\033[1;{colors.get(random.choice(e))}40me",end="")
-    print(f"\033[1;{colors.get(random.choice(e))}40m?",end="")
-    input("\n(Y/N)\n")
+    print(f"\033[1;{random.choice(list(colors.values()))}0mG",end="")
+    print(f"\033[1;{random.choice(list(colors.values()))}0ma",end="")
+    print(f"\033[1;{random.choice(list(colors.values()))}0mm",end="")
+    print(f"\033[1;{random.choice(list(colors.values()))}0me",end="")
+    print(f"\033[1;{random.choice(list(colors.values()))}0m?",end="")
+    print("\033[0m",end="")
+    play = input("\n(Y/N)\n").lower()
 
-    if play == "Y":
+    if play == "y":
         start_game()
-    elif play == "N":
+    elif play == "n":
         for i in range(5):
             world.clear()
             print(f"\033[1;{colors.get(random.choice(e))}40mT",end="")
@@ -89,6 +93,6 @@ def title_screen():
 def start_game():
     world.display(grid)
     while True:
-        move_player()
+        world.move_player(grid)
 
 title_screen()
