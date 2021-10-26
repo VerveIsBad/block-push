ea9be6d7b0cd020f9228cd5dd8e7b67ce56c1aae
Christopher Truluck <ctruluck12107@gmail.com>
2021-10-26 19:25:40 +0000

    Null

--- dev.py
+++ dev.py
@@ -3,37 +3,3 @@ import random
 import sys
 import os
 from subprocess import call
-
-
-
-def clear():
-    """
-    Clears the terminal.
-    """
-    call('clear' if os.name =='posix' else 'cls')
-
-
-
-
-def win_text():
-    colors = {
-        "red":31,
-        "yellow":33,
-        "green":32,
-        "blue":34,
-        "purple":35
-    }
-
-    e = ['red','yellow','green','blue','purple']
-
-    for i in range(4):
-        clear()
-        print(f"\033[1;{colors.get(random.choice(e))};40mY",end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mO",end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mU",end="")
-        print(f" ", end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mW",end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mI",end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mN",end="\n")
-        sleep(0.3)
-win_text()
