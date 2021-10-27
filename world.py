9d34b38d91da783213b1019d370099209ee1896e
Christopher Truluck <ctruluck12107@gmail.com>
2021-10-27 16:14:58 +0000

    broken, working on it

--- world.py
+++ world.py
@@ -9,23 +9,18 @@ from time import sleep
 ranY = 0
 ranX = 0
 
- # Creates a instance of PLAYER and BOX
+# Creates a instance of PLAYER and BOX
 player = Person(None,None,"⊠")
-box = Person(ranX,ranY)
+box = Person(ranX,ranY, "▣")
 
 # Creates a instance of the GOAL
-goal = Person(ranX,ranY)
-
-box.icon = "▣"
-goal.icon = "▢"
+goal = Person(ranX,ranY, "▢")
 
 def clear():
     """
     Clears the terminal.
     """
     call('clear' if os.name =='posix' else 'cls')
-
-
 
 def create_grid(rows = 7, collums = 9):
     """
@@ -39,7 +34,6 @@ def create_grid(rows = 7, collums = 9):
     else:
         player.y = random.randint(1,rows)
         player.x = random.randint(1, collums)
-    # \033[1;36;40m
 
     grid = [] # defines grid 
 
@@ -66,20 +60,21 @@ def create_play_state(grid, rows = 7, collums = 9):
     Coding is stupid. Fuck my life.
     """
 
-    box.x = random.randint(1, collums-1)
-    box.y = random.randint(1,rows-1)
+    box.x = random.randint(1, collums-1) # Randomize cordinates 
+    box.y = random.randint(1,rows-1) # for box and goal 
 
     goal.y = random.randint(1, rows-1)
     goal.x = random.randint(1, collums-1)
 
+    # checks for coordinate overlays 
     if box.y == player.y and box.x == player.x or player.y == goal.y and player.x == goal.x or box.x == goal.x and goal.y == box.y:
         create_play_state(grid)
     else:
         pass
 
-    grid[player.y][player.x] = f"\033[1;36;40m{player.icon}\033[0m"
-    grid[box.y][box.x] = f"\033[1;31;40m{box.icon}\033[0m" # adds box
-    grid[goal.y][goal.x] = f"\033[1;34;40m{goal.icon}\033[0m" # adds goal
+    grid[player.y][player.x] = f"\033[1;36;10m{player.icon}\033[0m" # adds player
+    grid[box.y][box.x] = f"\033[1;31;10m{box.icon}\033[0m" # adds box
+    grid[goal.y][goal.x] = f"\033[1;34;10m{goal.icon}\033[0m" # adds goal
 
 def win_text():
     colors = {
@@ -94,13 +89,9 @@ def win_text():
 
     for i in range(6):
         clear()
-        print(f"\033[1;{colors.get(random.choice(e))};40mY",end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mO",end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mU",end="")
-        print(f" ", end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mW",end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mI",end="")
-        print(f"\033[1;{colors.get(random.choice(e))};40mN",end="\n")
+        print(f"\033[1;{colors.get(random.choice(e))};10mY\033[0m\033[1;{colors.get(random.choice(e))};10mo\033[0m\033[1;{colors.get(random.choice(e))};0mu\033[0m",end="")
+        print(f" ", end="") # \033[0m
+        print(F"\033[1;{colors.get(random.choice(e))};10mw\033[0m \033[1;{colors.get(random.choice(e))};10mi\033[0m \033[1;{colors.get(random.choice(e))};10mn\033[0m \033[1;{colors.get(random.choice(e))};10m!\033[0m")
         sleep(0.2)
 
 def check_win_status(box,goal):
@@ -113,15 +104,58 @@ def check_win_status(box,goal):
         print()
         sys.exit()   
 
-def display(grid):
+def display(grid, debug = False):
     """
     Displays the grid.
     """ 
     clear()
+    if debug == True:
+        print(f"PLAYER COORDS, Y:{player.y} X:{player.x}")
+        print(f"BOX COORDS, Y:{box.y} X:{box.x}")
+        print(f'GOAL COORDS, Y:{goal.y} X:{goal.x}')
+    else:
+        pass
+
     for line in grid:
         print(' '.join(map(str,line)))
 
-def move_player(grid, rows = 7, collums = 9, Debug = False):
+def check_for_errors(grid,player,box):
+    """
+    Checks if the player or box
+    Goes past the grid.
+    """
+    updating = True
+    while updating:
+
+        try: # UwU Whats this~? Oh its your error handling ~~  
+            # adds player and box to grid with updated position 
+            grid[player.y][player.x] = f"\033[1;36;10m{player.icon}\033[0m"
+            grid[box.y][box.x] = f"\033[1;31;10m{box.icon}\033[0m" # adds box
+            check_win_status(box, goal) 
+            updating = False 
+        except IndexError: # i-its so big~~
+            # POSSIBLE PLAYER COORDINATE ERRORS
+            if player.y == rows:
+                player.y = 0
+            elif player.y < 0:
+                player.y = rows - 1
+            elif player.x == collums:
+                player.x = 0
+            elif player.x < 0:
+                player.x = collums - 1
+            # POSSIBLE BOX COORDINATE ERRORS
+            elif box.y == rows:
+                box.y = 0
+            elif box.y < 0:
+                box.y = rows - 1
+            elif box.x == collums:
+                box.x = 0
+            elif box.x < 0:
+                box.x = collums - 1
+            else:
+                print("error")       
+
+def move_player(grid, rows = 7, collums = 9, debug = False):
     """
     Moves the player. Gets input
     sets current player pos to 0
@@ -167,7 +201,7 @@ def move_player(grid, rows = 7, collums = 9, Debug = False):
 
     elif user_move == "stop":
         sys.exit()
-    elif Debug == True or user_move == "Start debug":
+    elif debug == True or user_move == "start debug":
         # UwU You want to see the raw Positons ~ You sussy baka ~
         print(f"PLAYER COORDS, Y:{player.y} X:{player.x}")
         print(f"BOX COORDS, Y:{box.y} X:{box.x}")
@@ -175,40 +209,3 @@ def move_player(grid, rows = 7, collums = 9, Debug = False):
     else:
         print("Please select from original prompt\n (w,a,s,d)")
     
-    # Starts the update loop.
-    updating = True
-    
-    while updating:
-        """
-        Checks if the player or box
-        Goes past the grid.
-        """
-        try: # UwU Whats this~? Oh its your error handling ~~  
-            # adds player and box to grid with updated position 
-            grid[player.y][player.x] = f"\033[1;36;40m{player.icon}\033[0m"
-            grid[box.y][box.x] = f"\033[1;31;40m{box.icon}\033[0m" # adds box
-            check_win_status(box, goal) 
-            updating = False 
-        except IndexError: # i-its so big~~
-            # POSSIBLE PLAYER COORDINATE ERRORS
-            if player.y == rows:
-                player.y = 0
-            elif player.y < 0:
-                player.y = rows - 1
-            elif player.x == collums:
-                player.x = 0
-            elif player.x < 0:
-                player.x = collums - 1
-            # POSSIBLE BOX COORDINATE ERRORS
-            elif box.y == rows:
-                box.y = 0
-            elif box.y < 0:
-                box.y = rows - 1
-            elif box.x == collums:
-                box.x = 0
-            elif box.x < 0:
-                box.x = collums - 1
-            else:
-                print("error")       
-    
-        display(grid) # displays grid
